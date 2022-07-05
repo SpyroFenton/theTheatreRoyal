055b605af21402ccfb31f8e72444508c47845f4e
meganlabonte <mklabonte1@gmail.com>
2022-07-05 22:26:15 +0100

    Showtype
    subclasses added

---
+++ ShowType/src/showType.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class showType {
+	
+	
+	
+	public static void main(String[] args) {
+		
+	    ArrayList<String> performers = new ArrayList<String>();
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	}
+}
