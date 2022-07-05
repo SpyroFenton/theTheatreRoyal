80b88edfc511f2fbbf476a6195765c864911b2b5
meganlabonte <mklabonte1@gmail.com>
2022-07-05 22:29:53 +0100

    Show type variables

--- ShowType/src/showType.java
+++ ShowType/src/showType.java
@@ -3,16 +3,33 @@ import java.util.ArrayList;
 
 public class showType {
 	
+	ArrayList<String> performers;
+	String showName;
+	private String showType; //sorry unsure about dis
+	String duration;
+	String language;
+	int showProductionID;
+	boolean liveAccompaniment;
+	String description;
+	Double priceStall; 
+	Double priceCircle;
+	
+	//build show constructor 
+	public void buildShow() {
+		
+	}
+	
+	//getter 
+	
+	//setter 
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { //ignore
 		
 	    ArrayList<String> performers = new ArrayList<String>();
-	    performers.add("xxx");
-	    performers.add("xxx");
-	    performers.add("xxx");
-	    performers.add("xxx");
-	    performers.add("xxx");
-	    performers.add("xxx");
+	    performers.add("not adele");
+	    
 	}
+	
+	
 }
