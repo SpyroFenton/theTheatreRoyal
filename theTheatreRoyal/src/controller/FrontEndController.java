d647cb1c075d056d93cb9985fa30337529ba8f24
Fenton <62139484+SpyroFenton@users.noreply.github.com>
2022-07-06 10:38:03 +0200

    Added code from yesterdays Front End Controller

--- theTheatreRoyal/src/controller/FrontEndController.java
+++ theTheatreRoyal/src/controller/FrontEndController.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class FrontEndController {
+import util.InputReader;
 
+public class FrontEndController {
+	
+	public void start() {
+		private String choice = null;
+		
+		System.out.println("Welcome. Please choose either: Browse, search, date");
+		InputReader read = new InputReader();
+		
+		while (choice == null) {
+			String choice = read.getInputString();
+
+			if (choice.equalsIgnoreCase("Browse")) {
+				System.out.println("Browsing all shows");
+				choice = null;
+				
+			}
+
+			else if (choice.equalsIgnoreCase("Search")) {
+				System.out.println("Search for show");
+				choice = null;
+
+			}
+
+			else if (choice.equalsIgnoreCase("date")) {
+				System.out.println("Search by date");
+				choice = null;
+				
+				
+			} else if (choice.equalsIgnoreCase("exit")) {
+				System.out.println("Exiting");
+				break;
+			}
+
+			else {
+				System.out.println("Invalid choice");
+				choice = null;
+			}
+		}
+	}
 }
