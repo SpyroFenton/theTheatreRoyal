7d48d8728874ee54b8ea1100d13162b88c1f9172
Fenton <62139484+SpyroFenton@users.noreply.github.com>
2022-07-06 10:35:16 +0200

    Java Project recreated and old one discarded
    Input Reader, Front End, Show Type classes created.
    FrontEnd missing David's work from yesterday

--- theTheatreRoyal/src/controller/FrontEndController.java
+++ theTheatreRoyal/src/controller/FrontEndController.java
@@ -1,46 +1,5 @@
 package controller;
 
-import util.InputReader;
-
 public class FrontEndController {
-	
-	InputReader read = new InputReader();
-	private String choice = null;
 
-
-	public void start() {
-
-		System.out.println("Welcome. Please choose either: Browse, search, date");
-		
-		while (choice == null) {
-			String choice = read.getInputString();
-
-			if (choice.equalsIgnoreCase("Browse")) {
-				System.out.println("Browsing all shows");
-				choice = null;
-				
-			}
-
-			else if (choice.equalsIgnoreCase("Search")) {
-				System.out.println("Search for show");
-				choice = null;
-
-			}
-
-			else if (choice.equalsIgnoreCase("date")) {
-				System.out.println("Search by date");
-				choice = null;
-				
-				
-			} else if (choice.equalsIgnoreCase("exit")) {
-				System.out.println("Exiting");
-				break;
-			}
-
-			else {
-				System.out.println("Invalid choice");
-				choice = null;
-			}
-		}
-	}
 }
