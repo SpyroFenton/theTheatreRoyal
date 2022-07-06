50a59d043e3128ba031b77cf66dd1086de5b873d
hitenp93 <108623704+hitenp93@users.noreply.github.com>
2022-07-06 16:13:12 +0100

    completed first M
    added query to list shows, search for shows by name and date

--- theTheatreRoyal/src/controller/FrontEndController.java
+++ theTheatreRoyal/src/controller/FrontEndController.java
@@ -2,59 +2,95 @@ package controller;
 
 import util.DBConnector;
 import util.InputReader;
-import java.io.File;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 // import model.Person;
 
 public class FrontEndController {
-	
+
+	public FrontEndController() {
+	}
+
 	public static void main(String[] args) {
 		DBConnector db = new DBConnector();
+		InputReader in = new InputReader();
 		// connect to the database
 		db.connect();
-		
-		// method to see if showproduction lists
-		db.selectshowProduction();
-		
-		// method to list shows by name
-		db.searchShowByName();
-	}
-	
 
-		
-	
-		
-		
-		/*
-		 * public void start() { private String choice = null;
-		 * 
-		 * System.out.println("Welcome. Please choose either: Browse, search, date");
-		 * InputReader read = new InputReader();
-		 * 
-		 * while (choice == null) { String choice = read.getInputString();
-		 * 
-		 * if (choice.equalsIgnoreCase("Browse")) {
-		 * System.out.println("Browsing all shows"); choice = null;
-		 * 
-		 * }
-		 * 
-		 * else if (choice.equalsIgnoreCase("Search")) {
-		 * System.out.println("Search for show"); choice = null;
-		 * 
-		 * }
-		 * 
-		 * else if (choice.equalsIgnoreCase("date")) {
-		 * System.out.println("Search by date"); choice = null;
-		 * 
-		 * 
-		 * } else if (choice.equalsIgnoreCase("exit")) { System.out.println("Exiting");
-		 * break; }
-		 * 
-		 * else { System.out.println("Invalid choice"); choice = null; } }
-		 */
-	
+		// method to list all shows
+		// db.listShowProduction();
+
+		// method to list shows by name
+		// db.searchShowByName();
+
+		// method to list shows by date
+		// db.searchShowByDate();
+		printGreeting();
+
+		String choice = null;
+		while (choice == null) {
+
+			choice = in.getText("Please choose an option: 1.List shows, 2.Search by name, 3.Search by date");
+			if (choice.equals("1")) {
+				db.listShowProduction();
+			}
+			if (choice.equals("2")) {
+				db.searchShowByName();
+			}
+			if (choice.equals("3")) {
+				db.searchShowByDate();
+			} else {
+				// System.out.println("Invalid choice");
+				choice = null;
+			}
+
+		}
+	}
+
+	public static void printGreeting() {
+		System.out.println("Welcome");
+	}
+
+	public static void start() {
+		printGreeting();
+		DBConnector db = new DBConnector();
+		InputReader read = new InputReader();
+
+		String choice = null;
+
+		while (choice == null) {
+
+			choice = read.getText("Please choose an option: 1.List shows, 2.Search by name, 3.Search by date");
+			if (choice.equals("1")) {
+				db.listShowProduction();
+			}
+
+		}
+	}
 }
+/*
+ * public void start() { private String choice = null;
+ * 
+ * System.out.println("Welcome. Please choose either: Browse, search, date");
+ * InputReader read = new InputReader();
+ * 
+ * while (choice == null) { String choice = read.getInputString();
+ * 
+ * if (choice.equalsIgnoreCase("Browse")) {
+ * System.out.println("Browsing all shows"); choice = null;
+ * 
+ * }
+ * 
+ * else if (choice.equalsIgnoreCase("Search")) {
+ * System.out.println("Search for show"); choice = null;
+ * 
+ * }
+ * 
+ * else if (choice.equalsIgnoreCase("date")) {
+ * System.out.println("Search by date"); choice = null;
+ * 
+ * 
+ * } else if (choice.equalsIgnoreCase("exit")) { System.out.println("Exiting");
+ * break; }
+ * 
+ * else { System.out.println("Invalid choice"); choice = null; } }
+ */
--- theTheatreRoyal/src/util/DBConnector.java
+++ theTheatreRoyal/src/util/DBConnector.java
@@ -3,17 +3,12 @@ package util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Scanner;
-
-import util.InputReader;
 // import model.Person; our model
 
 public class DBConnector {
@@ -49,67 +44,91 @@ public class DBConnector {
 		}
 
 		if (conn != null) {
-			System.out.println("Connection established.");
+			// System.out.println("Connection established.");
 		} else {
 			System.out.println("Connection null still.");
 			System.exit(1);
 		}
 	}
 
-	
 	/**
 	 * select everything from the Person table, convert to a list of Person objects
 	 * and print each Person object.
 	 */
-	public void selectshowProduction() {
-		
+	public void listShowProduction() {
+
 		try {
 			// Create a statement
-			Statement myStmt =  conn.createStatement();
-			
+			Statement myStmt = conn.createStatement();
+
 			// Execute SQL query
 			ResultSet myRs = myStmt.executeQuery("select * FROM showProduction");
-			
+
 			// Process the result set
 			while (myRs.next()) {
-				System.out.println(myRs.getString("showName"));
+				System.out.println("Show name: " + myRs.getString("showName"));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		}
-	
+
+	}
+
 	public void searchShowByName() {
-		
+
 		try {
 			// Prepare a statement
-			myStmt =  conn.prepareStatement("SELECT\r\n"
-					+ "showProduction.showName,\r\n"
-					+ "showProduction.duration,\r\n"
-					+ "performance.showDate,\r\n"
-					+ "performance.showStartTime\r\n"
-					+ "FROM showProduction\r\n"
-					+ "INNER JOIN performance\r\n"
-					+ "ON showProduction.id = performance.showProductionID  \r\n"
-					+ "WHERE showProduction.showName = ?;");
-			
+			myStmt = conn
+					.prepareStatement("SELECT\r\n" + "showProduction.showName,\r\n" + "showProduction.duration,\r\n"
+							+ "performance.showDate,\r\n" + "performance.showStartTime\r\n" + "FROM showProduction\r\n"
+							+ "INNER JOIN performance\r\n" + "ON showProduction.id = performance.showProductionID  \r\n"
+							+ "WHERE showProduction.showName = ?;");
+
 			// Set the parameters
 			myStmt.setString(1, in.getText("Enter show name: "));
-			
+
 			// Execute SQL query
 			myRs = myStmt.executeQuery();
-			
+
 			// Display the result set
 			while (myRs.next()) {
-				System.out.println(myRs.getString("showName") + " " + myRs.getString("duration"));
-			
+				System.out.println(
+						"Show name: " + myRs.getString("showName") + " Duration: " + myRs.getString("duration"));
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		}
+
 	}
+
+	public void searchShowByDate() {
+
+		try {
+			// Prepare a statement
+			myStmt = conn.prepareStatement("SELECT\r\n" + "showProduction.showName,\r\n"
+					+ "showProduction.duration,\r\n" + "performance.showDate,\r\n" + "performance.showStartTime\r\n"
+					+ "FROM showProduction\r\n" + "INNER JOIN performance\r\n"
+					+ "ON showProduction.id = performance.showProductionID  \r\n" + "WHERE performance.showDate = ?;");
+
+			// Set the parameters
+			myStmt.setString(1, in.getText("Enter show date in YYYY-MM-DD: "));
+
+			// Execute SQL query
+			myRs = myStmt.executeQuery();
+
+			// Display the result set
+			while (myRs.next()) {
+				System.out.println(
+						"Show name: " + myRs.getString("showName") + " Duration: " + myRs.getString("duration"));
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
