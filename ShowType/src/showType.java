836ad9b4f23ff607a069f1d6d44d79966cd127d7
Fenton <62139484+SpyroFenton@users.noreply.github.com>
2022-07-06 09:45:44 +0200

    Revert "Show type variables"
    This reverts commit 80b88edfc511f2fbbf476a6195765c864911b2b5.

--- ShowType/src/showType.java
+++ ShowType/src/showType.java
@@ -3,33 +3,16 @@ import java.util.ArrayList;
 
 public class showType {
 	
-	ArrayList<String> performers;
-	String showName;
-	private String showType; //sorry unsure about dis
-	String duration;
-	String language;
-	int showProductionID;
-	boolean liveAccompaniment;
-	String description;
-	Double priceStall; 
-	Double priceCircle;
-	
-	//build show constructor 
-	public void buildShow() {
-		
-	}
-	
-	//getter 
-	
-	//setter 
 	
 	
-	public static void main(String[] args) { //ignore
+	public static void main(String[] args) {
 		
 	    ArrayList<String> performers = new ArrayList<String>();
-	    performers.add("not adele");
-	    
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
+	    performers.add("xxx");
 	}
-	
-	
 }
